41cfb5b10d8a5a08c7a4fd77dc6fbf87224ce881
paki1019 <paki1019@gmail.com>
2021-04-10 20:36:58 +0900

    initialize project

---
+++ src/main/java/jh/atelier/youtubethumbnailscrapper/YoutubeThumbnailScrapperApplication.java
@@ -0,0 +1,13 @@
+package jh.atelier.youtubethumbnailscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeThumbnailScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeThumbnailScrapperApplication.class, args);
+	}
+
+}
